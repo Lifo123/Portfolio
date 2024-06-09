a28d9f0d691c3fa669e5735569a8b8b2748ce558
Lifo123 <alberto_p-@hotmail.com>
2024-06-08 22:02:35 -0500

    Message Dev

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import { BrowserRouter, HashRouter } from "react-router-dom"
 //Lazy imports
 import LoadingBar from "react-top-loading-bar"
 import userDarkMode from "./hooks/useDarkMode/useDarkMode";
+import MessageDev from "./Components/MessageDev/MessageDev";
 const AppRoutes = lazy(() => import("./Routes/AppRoutes"))
 
 function App() {
-  const {ToggleDM} = userDarkMode();
+  const { ToggleDM } = userDarkMode();
   const { progress, setProgress } = useLoad(0);
 
   return (
     <BrowserRouter basename="/Portfolio/">
       <Suspense fallback={<p>Loading...</p>}>
         <LoadingBar color="#2388EE" progress={progress} onLoaderFinished={() => setProgress(0)} height={5} />
+        <MessageDev />
         <AppRoutes />
       </Suspense>
     </BrowserRouter>
---
+++ src/Components/MessageDev/MessageDev.jsx
@@ -0,0 +1,9 @@
+ import './MessageDev.css'
+ 
+export default function MessageDev(){
+   return (
+       <div className="msg-dev d-flex f-center">
+           Website Under Development
+       </div>
+   )
+}
