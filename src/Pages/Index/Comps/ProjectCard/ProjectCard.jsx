1be891cbd94f568db1a0372704ec22e57abd9f8c
Lifo123 <alberto_p-@hotmail.com>
2024-06-09 22:07:52 -0500

    Optimizing Cards

--- src/Pages/Index/Comps/ProjectCard/ProjectCard.jsx
+++ src/Pages/Index/Comps/ProjectCard/ProjectCard.jsx
@@ -2,26 +2,30 @@ import './Styles/ProjectCard.css'
 import Icon from '../../../../Components/icons/Icon'
 import SocialMediaIcon from '../../../../Components/icons/SocialMediaIcons'
 
-export default function ProjectCard({ }) {
+export default function ProjectCard({ data }) {
     return (
         <div className='project-card br-10 o-hidden'>
-            <span className='d-flex f-center relative'>
+            <a className='d-flex f-center relative' href={data.websiteURL}>
                 <span className='p-c-ic-e absolute'>
                     <Icon id={'redirect'} box={26} />
                 </span>
                 <img src="" height={140} width={340} alt="img Project" />
-            </span>
+            </a>
             <div className='p-c-info f-col f-align-start g-5'>
-                <h3>Meta Tag Editor</h3>
-                <p className=''>Simple editor of metatag for testing the result in engines Searchs</p>
+                <h3>{data.tittle || 'Tittle'}</h3>
+                <p className=''>{data.description || 'Description'}</p>
                 <div className='f-row g-25 f-justify-between mt-10 w-100'>
-                    <a className='br-max f-row g-15 f-align-center' href='https://github.com/Lifo123' target='_blank'>
-                        <SocialMediaIcon id={'github'} />
-                        <p>Github</p>
+                    <a className='p-btn br-max f-row g-15 f-align-center' href={data.repoURL} target='_blank'>
+                        <label className='f-row f-center br-max g-10'>
+                            <SocialMediaIcon id={'github'} />
+                            <p>Github</p>
+                        </label>
                     </a>
-                    <a className='br-max f-row g-15 f-align-center' href='https://www.linkedin.com/in/lifo3/' target='_blank'>
-                        <Icon id={'redirect'} box={18} />
-                        <p className='d-flex f-align-center' style={{ height: '30px' }}>Website</p>
+                    <a className='p-btn br-max f-row g-15 f-align-center' href={data.websiteURL} target='_blank'>
+                        <label className='f-row f-center br-max g-10'>
+                            <Icon id={'redirect'} box={18} />
+                            <p className='d-flex f-align-center' style={{ height: '30px' }}>Website</p>
+                        </label>
                     </a>
                 </div>
             </div>
