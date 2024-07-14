32d5fc4a89ff0ca44cfc2092a31eebf645c94f11
Lifo123 <alberto_p-@hotmail.com>
2024-07-14 14:11:20 -0500

    Updates

---
+++ assets/AllProjects-ChCkMrgF.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DCLJseLA.js";import"./AppRoutes-DOyc2k0P.js";function r(){return t.jsx("main",{children:"Testing a Google Index"})}export{r as default};
--- assets/AllProjects-DTcDSRXf.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DEfBPLsv.js";import"./AppRoutes-DupWbumr.js";function r(){return t.jsx("main",{children:"Testing a Google Index"})}export{r as default};
