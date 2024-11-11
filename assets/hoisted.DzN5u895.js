c684fcb3929becb7b55ef78d8d6f2c38f8279f8a
Lifo123 <alberto_p-@hotmail.com>
2024-11-11 15:35:32 -0500

    Updates

---
+++ assets/hoisted.DzN5u895.js
@@ -0,0 +1,1 @@
+import"./hoisted.6Ydpkoc1.js";const e=new Date().getFullYear()-2003;document.getElementById("age-show").innerHTML=`${e}`;
--- src/assets/icons/Technologies/Index.ts
+++
@@ -1,9 +0,0 @@
-export { default as AstroIcon } from "./AstroIcon.astro";
-export { default as CSS } from "./CSS.astro";
-export { default as Git } from "./Git.astro";
-export { default as HTML } from "./HTML.astro";
-export { default as JS } from "./JS.astro";
-export { default as React } from "./React.astro";
-export { default as Sass } from "./Sass.astro";
-export { default as NodeJS } from "./NodeJS.astro";
-export { default as Firebase } from "./Firebase.astro";
