41fad03e47450dcd157ef66f8b1bcd0a7456b183
Lifo123 <alberto_p-@hotmail.com>
2024-11-11 15:48:03 -0500

    Updates

---
+++ assets/hoisted.BIs-s5Fh.js
@@ -0,0 +1,1 @@
+import"./hoisted.6Ydpkoc1.js";const o=document.cookie.split(";").find(i=>i.startsWith("F-User="));o&&(window.location.href="/Admin/Dashboard");
--- assets/hoisted.BfgOYdWB.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.6Ydpkoc1.js";const i=document.cookie.split(";").find(o=>o.startsWith("F-User="));i&&(window.location.href="/Portfolio/Admin/Dashboard");
