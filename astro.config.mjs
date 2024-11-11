a5c80d29e8eadf729dda57bdc9be2d74e5ec6292
Lifo123 <alberto_p-@hotmail.com>
2024-11-11 15:19:11 -0500

    Subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lifo123.site/',
+  site: 'https://www.lifo123.site/',
   integrations: [react(), mdx(), sitemap()],
   outDir: 'Build',
   build: {
